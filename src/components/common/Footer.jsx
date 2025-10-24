cf4ce6af2961e09e6b2855b9cea9d83038fb6405
saiful3278G <mdm439382@gmail.com>
2025-10-24 23:21:06 +0800

    h

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <img 
+        src="/src/assets/footer_logo.svg" 
+        alt="Footer Logo" 
+        className="footer-logo"
+      />
+    </footer>
+  );
+};
+
+export default Footer;
